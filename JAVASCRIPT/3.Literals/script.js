7617cf8476c3c551ae8fc5bbd436583d4201495c
Shivam Tejani <sq.shivam4106@gmail.com>
2024-12-24 17:03:40 +0530

    24-12-2024

---
+++ JAVASCRIPT/3.Literals/script.js
@@ -0,0 +1,84 @@
+/* Javascript Template Literals */
+
+{
+    // let str1 = 'This is Javascript'
+    // let str2 = "This is Javascript"
+    // let str3 = `This is Javascript`
+
+    // console.log(str1);
+    // console.log(str2);
+    // console.log(str3);
+
+    // let str4 = 'This is "Javascript"'
+    // let str5 = "This is `Javascript`"
+    // let str6 = `This is "Javascript"`
+    
+    // console.log(str4);
+    // console.log(str5);
+    // console.log(str6);
+
+    // let names = 'SkillQode'
+
+    // let str7 = `This is ${names} institute`
+
+    // console.log(str7);
+}
+
+// String Object
+
+{
+
+    // let names = 'SkillQode'
+
+    // let str = new String(`This is ${names} String Object`)
+
+    // console.log(str);
+    
+}
+
+// String Operator 
+
+{
+    // let str = 'Javascript' + 'Lecture' // concatinating
+
+    // console.log(str);
+
+    // let str1 = 'Javascript' - 'Lecture'
+
+    // console.log(str1);
+
+    // let math = 100 + 100 + 'Javascript' + 100 + 100 - 100 - 100
+
+    // console.log(math);
+
+    // let math1 = 'Javascript' + 100 + 200 + 100 - 100 * 100
+
+    // console.log(math1);
+    
+    // let math2 = 'Javascript' + 100 + ((100 - 100) - 100) - 100
+    
+    // console.log(math2);
+
+    // let math3 = (100 - 100 + 'Javascript' + 100 + 100 + 100) + 100 - 100
+
+    // console.log(math3);
+
+    // let math4 = "100"  + 100 - 100 - 100
+
+    // console.log(math4);
+    
+}
+
+/* TypesOf Operator */
+
+/*
+1. Arithmetic
+2. Assigement
+3. Logical
+4. Comparision
+5. Ternary
+6. typeOf
+7. instanceOf
+8. Bitwise 
+9. ++ , -- [ ]
+*/
