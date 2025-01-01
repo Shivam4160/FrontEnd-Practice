ff94d8044cda138558d61f536e500f5dcc535a69
Shivam Tejani <sq.shivam4106@gmial.com>
2025-01-01 18:29:54 +0530

    1-1-2025

--- JAVASCRIPT/Z.Prectice/Loops/script.js
+++ JAVASCRIPT/Z.Prectice/Loops/script.js
@@ -59,39 +59,101 @@
 
 
 
-// new 
+// new (custom console)
+
+
+// {
+
+//     var customConsole = {
+//         // Store a reference to your backing value
+//         tempValue: '',
+//         // Always write out the most recent value
+//         log: function(msg) {
+//           this.tempValue += msg;
+//           console.clear();
+//           console.log(this.tempValue);
+//         },
+//         // A clear method to clear the backing console
+//         clear: function() {
+//           this.tempValue = '';
+//           console.clear();
+//         }
+//       }
+
+
+//     let fibo=10
+//     let fibbonaci=0;
+//     let num1=0 , num2=1
+
+//     for (let i = 1; i <=fibo; i++) {
+//         fibbonaci=num1;
+//         num1= num2;
+//         num2= num1 + fibbonaci;
+//         customConsole.log(fibbonaci+",");
+//     }
+    
+   
+
+// }
+
+
+
+
+// Write a program that reverses a given string.
+
+
 
 
 {
 
-    var customConsole = {
-        // Store a reference to your backing value
-        tempValue: '',
-        // Always write out the most recent value
-        log: function(msg) {
-          this.tempValue += msg;
-          console.clear();
-          console.log(this.tempValue);
-        },
-        // A clear method to clear the backing console
-        clear: function() {
-          this.tempValue = '';
-          console.clear();
-        }
-      }
-
-
-    let fibo=10
-    let fibbonaci=0;
-    let num1=0 , num2=1
-
-    for (let i = 1; i <=fibo; i++) {
-        fibbonaci=num1;
-        num1= num2;
-        num2= num1 + fibbonaci;
-        customConsole.log(fibbonaci+",");
+    const string = "Hello, world!";
+    let reversedStr = '';
+    
+    for (let i = string.length - 1; i >= 0; i--) {
+        reversedStr += string[i];
     }
     
-   
+    console.log(reversedStr);
+}
+ 
 
+
+
+// Write a program that calculates the sum of all elements in an array
+
+
+{
+
+    const numbers = [1, 2, 3, 4, 5 , 6 , 7 , 8 , 9];
+    
+    let sum = 0;
+    for (let i = 0; i < numbers.length; i++) {
+      sum += numbers[i];
+    }
+    console.log(sum);
+      
+
+}
+
+
+
+
+
+// Write a program that finds the largest number in an array.
+
+
+{
+    
+    
+    const numbers = [3, 5, 7, 2, 8, 16 , 12];
+    let largest =numbers[0];
+
+    for (let i = 1; i < numbers.length; i++) {
+      if (numbers[i] > largest) {
+        largest = numbers[i];
+      }
+    }
+    
+    console.log(largest); 
+      
 }
