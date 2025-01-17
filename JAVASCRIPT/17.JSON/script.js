60fa4e590369ea29aa5e9fd2375d171d5f916205
Shivam Tejani <sq.shivam4106@gmial.com>
2025-01-17 17:30:32 +0530

    13/01/2025

---
+++ JAVASCRIPT/17.JSON/script.js
@@ -0,0 +1,39 @@
+/* Javascript Object Notation */
+
+
+/* JSON */
+
+{
+    let obj = { name:'bansi' , gender:'female' , age:24 }
+  
+    console.log(obj);
+  
+    let jsonObj = JSON.stringify(obj)
+    console.log(jsonObj);
+  }
+  
+  
+  {
+    let obj = '{"name":"bansi","gender":"female","age":24}'
+  
+    console.log(obj);
+  
+    let parseObj = JSON.parse(obj)
+  
+    console.log(parseObj);
+  }
+  
+  // javascript fetch
+  
+  {
+    async function profileData(){
+      let res = await fetch('http://localhost:3000/profile')
+      let data  = await res.json()
+      console.log(data[0].name);
+      console.log(data[0].gender);
+      console.log(data[0].age);
+      console.log(data[0].Details[0].eye);
+    }
+  
+    profileData()
+  }
