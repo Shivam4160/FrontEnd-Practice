7ce812005297ef54b0a978ed9af7eb66b494185e
Shivam Tejani <sq.shivam4106@gmial.com>
2025-01-17 18:51:51 +0530

    17/01/2025

--- JAVASCRIPT/18.Functions/script.js
+++ JAVASCRIPT/18.Functions/script.js
@@ -9,3 +9,11 @@ let arr = [4 , 32]
 let arr2 = [ 15 , 9]
 
 console.log(sum(...arr, ...arr2));
+
+// JavaScript Arrow Function 
+
+let sum2 = (a , b , c , d) =>{
+    return a / b * c + d 
+}
+
+console.log(sum2(...arr, ...arr2));
