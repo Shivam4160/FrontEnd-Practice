a930440e1a592902a836aa85565281bf028459c5
Shivam Tejani <sq.shivam4106@gmial.com>
2025-02-01 18:22:22 +0530

    01/02/2025

---
+++ JAVASCRIPT/29.FunctionClosure/script.js
@@ -0,0 +1,79 @@
+// Javascript Function Closure
+
+
+// function sum(){
+//     return 4 + 4
+// }
+
+// console.log(sum());
+
+// {
+//     function print(name){
+//         function greet(){
+//             console.log('name is' , name);
+//         }
+//         greet();
+//     }
+    
+//     print('skillQode')
+// }
+
+// {
+//     function print(name){
+//         function greet(){
+//             console.log('name is' , name);
+//         }
+//         return greet;
+//     }
+    
+//     let demo = print('skillQode')
+
+//     console.log(demo);
+
+//     console.log(demo());
+// }
+// {
+//     function sum(){
+
+//         let num = 0
+
+//         function multi(){
+//            return num = num + 1
+//         }
+
+//         return multi();
+//     }
+    
+//     console.log(sum()); 
+//     console.log(sum()); 
+// }
+
+// {
+//     function multiply(x){
+//         function add(y){
+//             return x * y
+//         }
+//         return add;
+//     }
+
+//     // let demo = multiply(1)
+//     let demo = multiply(2)
+//     let demo2 = multiply(2)
+//     console.log(demo(2));
+//     console.log(demo2(6));
+// }
+
+
+// {
+//     if (Math.random() > 0.5) {
+//         var x = 1;
+//       } else {
+//         var x = 2;
+//       }
+//       console.log(x);
+      
+// }
+
+
+
+
